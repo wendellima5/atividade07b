c7c5d57ce410eb310477962030d30d619635be27
wendellima5 <w6805977@gmail.com>
2024-10-21 15:30:56 -0500

    atividade

---
+++ js/jquery-script.js
@@ -0,0 +1,11 @@
+$(Document).ready(function(){
+
+    $("#botao-cadastrar").click(function(){
+    
+        $("#form-cadastrar").slideToggle("slow");
+        $("#section-login").slideToggle("slow");
+        $("#botao-cadastrar").hide();
+    
+    });
+    
+    });
